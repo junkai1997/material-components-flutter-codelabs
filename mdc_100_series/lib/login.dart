2001746dd7e5cb76ad1b56bedea95ab6ca7cfa09
seeto <seeto@seeto>
2023-12-28 18:47:31 +0800

    MDC-103 Flutter: Add Shape

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -78,7 +78,10 @@ class _LoginPageState extends State<LoginPage> {
                     _passwordController.clear();
                   },
                   style: TextButton.styleFrom(
-                    foregroundColor: Theme.of(context).colorScheme.secondary,
+                    foregroundColor: kShrineBrown900,
+                    shape: const BeveledRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                    )
                   ),
                 ),
                 // TODO: Add an elevation to NEXT (103)
@@ -93,6 +96,9 @@ class _LoginPageState extends State<LoginPage> {
                     foregroundColor: kShrineBrown900,
                     backgroundColor: kShrinePink100,
                     elevation: 8.0,
+                    shape: const BeveledRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                    )
                   )
                 ),
               ]),

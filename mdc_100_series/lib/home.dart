293896d2c979db96fb92ef4c74fa0ed659576eb6
seeto <seeto@seeto>
2024-02-19 15:53:55 +0800

    MDC-104 Flutter: Add a menu on the back layer

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -20,14 +20,19 @@ import 'model/products_repository.dart';
 import 'supplemental/asymmetric_view.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
   // TODO: Add a variable for Category (104)
+  final Category category;
+
+  const HomePage({
+    this.category = Category.all,
+    Key? key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     return AsymmetricView(
-        products: ProductsRepository.loadProducts(Category.all));
+        products: ProductsRepository.loadProducts(category));
     // TODO: Pass Category variable to AsymmetricView (104)
   }
 }
